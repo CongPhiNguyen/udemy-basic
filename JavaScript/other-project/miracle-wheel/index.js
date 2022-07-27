3c3a5ba6fa1fb1f237f4676571860ca3b3a54058
CongPhiNguyen <congphinguyen312@gmail.com>
2022-07-27 09:11:20 +0700

    basic miracle wheel

--- JavaScript/other-project/miracle-wheel/index.js
+++ JavaScript/other-project/miracle-wheel/index.js
@@ -1,12 +1,42 @@
 const wheelDOM = document.querySelector("#wheel");
-
+const options = [
+  "Mất lượt",
+  "100 điểm",
+  "200 điểm",
+  "Quà tặng",
+  "May mắn",
+  "Nhân đôi",
+  "Mất lượt",
+  "100 điểm",
+  "200 điểm",
+  "Quà tặng",
+  "May mắn",
+  "Nhân đôi",
+  "Mất lượt",
+  "100 điểm",
+  "200 điểm",
+  "Quà tặng",
+  "May mắn",
+  "Nhân đôi",
+];
 const createWheel = (part) => {
   for (let i = 0; i < part; i++) {
+    // Add text
+
+    // Add line
     const lineDOM = document.createElement("div");
     lineDOM.classList.add("line");
     lineDOM.style.transform = `rotate(${Math.round((360 * i) / part)}deg)`;
+    const textDOM = document.createElement("span");
+    textDOM.textContent = options[i];
+    textDOM.classList.add("text");
+    lineDOM.appendChild(textDOM);
     wheelDOM.appendChild(lineDOM);
+
+    // textDOM.style.top = "50%";
+    // textDOM.style.right = "0%";
+    // textDOM.style.transform = `rotate(${Math.round((360 * i) / part)}deg)`;
   }
 };
 
-createWheel(20);
+createWheel(18);
