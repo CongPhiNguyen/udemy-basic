44139dd217584979933f298d3b544a311f4e7fe4
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2022-07-28 04:01:12 +0700

    make accordition able to show all

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-var accItem = document.getElementsByClassName("accordion-item");
-var accHD = document.getElementsByClassName("accordion-item-title");
+const accItem = document.getElementsByClassName("accordion-item");
+const accHD = document.getElementsByClassName("accordion-item-title");
+
+function toggleItem() {
+  let itemClassList = this.parentNode.classList;
+  console.log(this.parentNode.classList);
+  // for (i = 0; i < accItem.length; i++) {
+  //   accItem[i].className = "accordion-item close";
+  // }
+  if (itemClassList.contains("open")) {
+    this.parentNode.classList.remove("open");
+    this.parentNode.classList.add("close");
+  } else if (itemClassList.contains("close")) {
+    this.parentNode.classList.remove("close");
+    this.parentNode.classList.add("open");
+  }
+  // if (itemClass == "accordion-item close") {
+  //   this.parentNode.className = "accordion-item open";
+  // }
+}
+
 for (i = 0; i < accHD.length; i++) {
   accHD[i].addEventListener("click", toggleItem, false);
-}
-function toggleItem() {
-  var itemClass = this.parentNode.className;
-  for (i = 0; i < accItem.length; i++) {
-    accItem[i].className = "accordion-item close";
-  }
-  if (itemClass == "accordion-item close") {
-    this.parentNode.className = "accordion-item open";
-  }
 }
